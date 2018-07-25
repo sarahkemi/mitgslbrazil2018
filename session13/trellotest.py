af9aa13480125f9b69bc0d3c4ea900b5a8f5c8de
sarahkemi <sarahaladetan@gmail.com>
2018-07-25 14:25:52 -0300

    clarified whats happening in trellotest

--- session13/trellotest.py
+++ session13/trellotest.py
@@ -1,21 +1,40 @@
 from config import config
 import requests
 
+# first, I try to get all the boards in my account
+
 board_endpoint = 'https://api.trello.com/1/members/me/boards'
 boards = requests.request("GET", board_endpoint, params=config).json()
 
 print len(boards)
 for board in boards:
-	print 'id:' + board['id']
-	print 'name:' + board['name']
-	print '\n'
+    print 'id:' + board['id']
+    print 'name:' + board['name']
+    print '\n'
 
-# #gsl bot board id is [removed]
+# second, I try to get all the lists from my board using the board id i found the codev above
+
 bid = ''
 list_endpoint = 'https://api.trello.com/1/boards/{}/lists'.format(bid)
 lists = requests.request("GET", list_endpoint, params=config).json()
 
 print lists
+
+# third I use the id of the list i want to add cards to make a post request to add this sample todo to the list
+
+list_id = ''
+card_endpoint = 'https://api.trello.com/1/cards'
+todo = 'i need to do this'
+query = {"name": todo, "pos": "top", "idList": list_id, "key": config['key'], "token": config['token']}
+
+# uncomment these following two lines to post the new card to the list
+
+# card_post = requests.request("POST", card_endpoint, params=query).json()
+# print card_post
+
+# success!
+
+# extra example to grab all the current cards on a list
 list_id = ''
 cards_endpoint = 'https://api.trello.com/1/lists/{}/cards?fields=id,name'.format(list_id)
 cards = requests.request("GET", cards_endpoint, params=config).json()
@@ -25,19 +44,6 @@ print cards
 tasks = []
 
 for card in cards:
-	tasks.append(card['name'])
+    tasks.append(card['name'])
 
 print tasks
-
-#desired list id is [removed]
-
-list_id = ''
-card_endpoint =  'https://api.trello.com/1/cards'
-todo = 'i need to do this'
-query = {"name":todo,"pos":"top","idList":list_id,"key":config['key'],"token":config['token']}
-
-# card_post = requests.request("POST", card_endpoint, params=query).json()
-
-# print card_post
-
-#success!
